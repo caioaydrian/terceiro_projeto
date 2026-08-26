5e2831e71b72ebe5228ce58be4f4df82531030b8
caioaydrian <caioaydrian42@gmail.com>
2026-08-26 20:48:46 -0300

    remove código desnecessário da api

--- api/dashboard.php
+++ api/dashboard.php
@@ -1,24 +1,6 @@
 <?php
 require_once "../config/database.php";
 
-// $lista_coffee_shop = [];
-// Busca bebidas e suas categorias
-// if (!empty($pdo)) {
-//     try {
-//         $sql = "SELECT d.id_drink, d.name, d.price, d.stock, c.id_category, c.name AS category
-//                 FROM drink d
-//                 JOIN category c ON d.id_category = c.id_category
-//                 ORDER BY c.name, d.name";
-//         $stmt = $pdo->query($sql);
-//         $lista_coffee_shop = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-//         echo json_encode($lista_coffee_shop);
-//     } catch (Exception $e) {
-//         error_log('Error on getting drinks: ' . $e->getMessage());
-//     }
-// }
-
-
 if (!empty($pdo)) {
 
     try {
